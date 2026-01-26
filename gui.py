d7d82ed809cb5712e623529d1f33441c4cbf25de
John35653 <johnresunenko@gmail.com>
2026-01-25 19:47:22 -0600

    im finally getting somewhere

--- gui.py
+++ gui.py
@@ -1,48 +1,46 @@
 import tkinter as tk
 import random
-from googletrans import Translator
-import backend   
+from googletrans import Translator   
 
-
+translator = Translator()
 colors = ["orange","black","white","blue","red","green", "yellow", "purple", "pink"]
 
 def colorChooser():
     color = random.choice(colors)
     root.config(bg=color)
-    instruction_label1.configure(text = f"This color in russian is called {root.cget('bg')}", font =("Times New Roman", 12))
-    
+    instruction_label1.configure(text = translator.translate(f"This color in russian is called {root.cget('bg')}\n What color is this in English?", dest='ru').text,\
+    font =("Times New Roman", 20))
 
+
+    
+title = translator.translate("Color Game",dest='ru').text
 
 
 #frontend logic:
 root = tk.Tk()    #use game_title_translation 
-root.title("Color Game")
-root.geometry("1000x500")
+root.title(f"{title}")
+root.geometry("1050x500")
 
 #experiment = colorChooser()
 root.configure(bg = "white")
                                                 #use label one translation    use ru_name
-instruction_label1 = tk.Label(root, text = f"This color in russian is called {root.cget('bg')}", font =("Times New Roman", 12))
+instruction_label1 = tk.Label(root, text = translator.translate(f"This color in russian is called {root.cget('bg')}\n What color is this in English?",dest='ru').text,\
+    font =("Times New Roman", 20))
 instruction_label1.pack(pady = 20)
 instruction_label1.place(x = 20, y = 40)
 
-instruction_label2 = tk.Label(root, text = "What color is this in english?", font = ("Times New Roman", 12))
-instruction_label2.pack(pady= 20)
-instruction_label2.place(x = 20, y = 20)
+#   entry = tk.Entry(root)
+#   entry.pack(pady = 20)
+#   entry.place(x=20, y=100)
+#   submit_button = tk.Button(root, text = "Submit")
+#   submit_button.pack(pady=20)
+#   submit_button.place(x= 150, y=100, width=50, height=20)
 
-entry = tk.Entry(root)
-entry.pack(pady = 20)
-entry.place(x=20, y=100)
+#user_answer = entry.get()
 
-submit_button = tk.Button(root, text = "Submit")
-submit_button.pack(pady=20)
-submit_button.place(x= 150, y=100, width=50, height=20)
-
-user_answer = entry.get()
-
-next_page = tk.Button(root, text = "Next", command=colorChooser)
+next_page = tk.Button(root, text = translator.translate("Next",dest='ru').text, font=("Tmes New Roman",20), command=colorChooser)
 next_page.pack(pady = 20)
-next_page.place(x = 950, y = 450)
+next_page.place(x = 850, y = 400)
 
 #previous_page = tk.Button(root, text = "Back")
 #previous_page.pack(pady = 20)
